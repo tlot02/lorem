0aa7e7057db2226daa1b51335843cb4101f55f8c
tlot02 <64542338+tlot02@users.noreply.github.com>
2021-03-11 14:10:19 -0500

    trying to dynamically show content using a form and a js array

--- app.js
+++ app.js
@@ -6,12 +6,21 @@ function showNav() {
 /////////////////////////////////////////////////
 create an array of objects. Use JS to dynamically produce the HTML from the array. Then I can use a form to submit to the array./////////////////////////////*/
 
-//Create the array
 let array = [];
-
-//Create a function to add a new item to the html.
 const addItem = () => {
   let string = "";
-  const headline = "";
-  const description = "";
+  const title = document.getElementById("title").value;
+  const description = document.getElementById("description").value;
+  array.push({ title: title, description: description });
+  let i = 0;
+  while (i < array.length) {
+    string += `<div class="flex-item flex-text invert">
+        <h2>${array[i].title}</h2>
+        <p>${array[i].description}<p>
+      </div>`;
+    i++;
+  }
+  document.getElementById("array").innerHTML = string;
 };
+
+// `<div class="number"><div><b>Name:</b>  ${array[i].name}</div><div><b>Job:</b>  ${array[i].job}</div><div><b>Phone:</b>  ${array[i].phone}</div></div>`;
