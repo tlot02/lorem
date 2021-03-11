62bf81156fd15477a69de74d1c6e1365ff5b7bfe
tlot02 <64542338+tlot02@users.noreply.github.com>
2021-03-11 13:40:58 -0500

    js

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 function showNav() {
   document.getElementsByClassName("navigation")[0].classList.toggle("active");
 }
+
+/*
+/////////////////////////////////////////////////
+create an array of objects. Use JS to dynamically produce the HTML from the array. Then I can use a form to submit to the array./////////////////////////////*/
+
+//Create the array
+let array = [];
+
+//Create a function to add a new item to the html.
+const addItem = () => {
+  let string = "";
+  const headline = "";
+  const description = "";
+};
