f87aeb135e65bb3b8032947ab78e5dcc8ad6a4ef
tlot02 <64542338+tlot02@users.noreply.github.com>
2021-03-31 21:19:07 -0400

    updates

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const addItem = () => {
   array.push({ title: title, description: description });
   let i = 0;
   while (i < array.length) {
-    string += `<div class="flex-item flex-text invert">
+    string += `<div id="content-${i}" class="flex-item flex-text invert">
+        <button id="rmv-btn-${i}" onClick="reply_click(this.id)" class="delete-btn">&#x2716;</button>
         <h2>${array[i].title}</h2>
         <p>${array[i].description}</p>
       </div>`;
@@ -21,7 +22,14 @@ const addItem = () => {
 };
 
 //need to create a dynamic ID first. Modify the code above.
+//Completed and working.
+
+//identify the ID of the  div when the button is clicked on
+
+//currently gives the id of the button (which is nothing). I need it to give the ID of the parent element
+function reply_click(clicked_id) {
+  console.log(clicked_id);
+  array.indexOf(clicked_id);
+}
 
 //Use Array.Splice to remove elements by index number. When clicking on the button, get the item's positon in teh array and remove it.
-
-//All to add a new event listen for onclick for each button element.
